d30dfdbf29350426a724c667f4accb20871d2d5a
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-12 17:18:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,14 @@
   #  phrase and break when the counter hits the appropriate number...
 
 def loop_iterator(number_of_times)
-  
+  counter = 0 
+  loop do 
+    counter <= number_of_times
+puts "Welcome to Flatiron School's Web Development Course!"
+counter += 1 
+if counter >= number_of_times
+  break 
+end 
 end
 
 
