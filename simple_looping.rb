acf5139e046b884d279624ed764dd4eb518008b4
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-12 17:02:28 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -20,7 +20,15 @@ phrase = "Welcome to Flatiron School's Web Development Course!"
 end
 
 
+counter = 0
+  loop do
+       counter <= number_of_times
+    puts "Welcome to Flatiron School's Web Development Course!"
+    counter += 1
+   	if counter >= number_of_times	        break
+  phrase = "Welcome to Flatiron School's Web Development Course!"	      end
 
+   end
 
 
 
