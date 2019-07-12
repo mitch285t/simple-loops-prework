8cfab05cf3136d24cd4b1af88fe3591779f36e53
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-12 17:19:29 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,37 +21,8 @@ end
 end
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
+  number_of_times.times 
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
