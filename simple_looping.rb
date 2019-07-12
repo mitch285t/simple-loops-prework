dc5f719653a270d742c951300f6bb2200acae502
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-12 17:17:35 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
   counter = 0 
   loop do 
     counter <= number_of_times
-puts"Welcome to Flatiron School's Web Development Course!"
+puts "Welcome to Flatiron School's Web Development Course!"
 counter += 1 
 if counter >= number_of_times
   break 
